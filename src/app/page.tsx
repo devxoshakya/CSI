d57d32a5f7990f0ac62ee5b5f535fb5c935da969
Dev Shakya <devxoshakya@gmail.com>
2024-12-31 22:54:38 +0530

    addded content thingy and removed eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 // import Image from "next/image";
 import Landing from "./home/page";
+import { getBlogPosts } from "@/data/events";
 
 
-export default function Home() {
+export default async function Home() {
+  const data = await getBlogPosts();
+  console.log(data);
   return (
     <div>
       
